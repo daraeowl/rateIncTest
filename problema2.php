70241e3f85a186a8ae50b3f090d43a6b424155e4
Jose Ruz <33108535+daraeowl@users.noreply.github.com>
2022-09-22 18:16:43 -0300

    Add files via upload

---
+++ problema2.php
@@ -0,0 +1,29 @@
+<!-- Dado 5 valores enteros positivos, encuentra los mínimos y máximos valores que pueden ser
+calculados sumando exactamente 4 de los 5 enteros. Luego imprime los respectivos minimo
+y maximo en una linea de 2 numeros separados. print mínimo + “ “ + máximo
+arr = [7, 3, 9, 1, 5]
+minimo = 1 + 3 + 5 + 7 = 16
+máximo = 3 + 5 + 7 + 9 = 24
+16 24
+
+Descripción de la función a desarrollar
+
+Debes completar la función miniMaxSum. Al final de la función debes imprimir el mínimo y
+máximo separados por un espacio.
+miniMaxSum recibe los siguientes parámetros:
+arr: un arreglo de 5 enteros
+
+Entrada de datos
+Una única línea con 5 números enteros separados por un espacio.
+Restricciones
+
+1 <= arr[i] <= 10^9 (1000000000)
+
+Formato de respuesta
+Debes imprimir el minimo y maximo separados por un espacio
+Javascript: console.log(min + “ “ + max)
+PHP: print(min . “ “ . max)
+input
+3 4 1 2 5
+output
+10 14 -->
---
+++ problema3.php
@@ -0,0 +1,29 @@
+<!-- Problema 3
+
+Dado un array de enteros el desafío es encontrar un elemento del array tal que la suma de
+todos los elementos de la izquierda sea igual a la suma de todos los elementos de la
+derecha
+arr = [5, 6, 8, 11]
+Elemento target es 8 ya que la suma de los elementos de la izquierda es igual a la suma de
+todos los elementos de la derecha, es decir, 5 + 6 == 11
+arr = [2, 0, 0, 0]
+El elemento target es 2 ya que la suma de los elementos de la izquierda es cero y la suma
+de todos los elementos de la derecha es cero igualmente.
+arr = [1]
+El elemento target es 1.
+
+Descripción de la función a desarrollar
+
+Debes completar la función balancedSums. Al final de la función debes retornar “YES” o
+“NO”
+“YES” = Es posible encontrar un elemento tal que la suma de todos los elementos de la
+izquierda sea igual a la de los elementos de la derecha.
+“NO” = No es posible encontrar un elemento tal que la suma de todos los elementos de la
+izquierda sea igual a la de los elementos de la derecha.
+Restricciones
+
+n = tamaño del array, 1 <= n <= 10^5
+arr[i] = valor de cada elemento, 1 <= arr[i] <= 2 * 10^4
+Formato de respuesta
+
+La función balancedSums debe retornar el string “YES” o “NO” en base a la descripción -->
