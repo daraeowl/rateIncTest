2bdbf2e126f20d837f273f92b30f754909460929
Jose Ruz <33108535+daraeowl@users.noreply.github.com>
2022-09-23 00:10:59 -0300

    Update problema2.php

--- problema2.php
+++ problema2.php
@@ -1,34 +1,27 @@
-<!--
+<?php
 /*
     Author: Jose Ruz
     Email: daraeowl@outlook.com
-/*-->
-<!-- Dado 5 valores enteros positivos, encuentra los mínimos y máximos valores que pueden ser
-calculados sumando exactamente 4 de los 5 enteros. Luego imprime los respectivos minimo
-y maximo en una linea de 2 numeros separados. print mínimo + “ “ + máximo
-arr = [7, 3, 9, 1, 5]
-minimo = 1 + 3 + 5 + 7 = 16
-máximo = 3 + 5 + 7 + 9 = 24
-16 24
+*/
 
-Descripción de la función a desarrollar
+/*
+ * Completa la funcion 'miniMaxSum' de abajo.
+ *
+ * La funcion acepta un INTEGER_ARRAY arr como unico parametro.
+ */
 
-Debes completar la función miniMaxSum. Al final de la función debes imprimir el mínimo y
-máximo separados por un espacio.
-miniMaxSum recibe los siguientes parámetros:
-arr: un arreglo de 5 enteros
+function miniMaxSum($arr) {
+    $min = 0;
+    $max = 0;
+    $sum = 0;
+    for ($i = 0; $i < count($arr); $i++) {
+        $sum += $arr[$i];
+    }
+    $min = $sum - max($arr);
+    $max = $sum - min($arr);
+    echo $min . "," . $max;
+}
 
-Entrada de datos
-Una única línea con 5 números enteros separados por un espacio.
-Restricciones
-
-1 <= arr[i] <= 10^9 (1000000000)
-
-Formato de respuesta
-Debes imprimir el minimo y maximo separados por un espacio
-Javascript: console.log(min + “ “ + max)
-PHP: print(min . “ “ . max)
-input
-3 4 1 2 5
-output
-10 14 -->
+$arr = array(7, 3, 9, 1, 5);
+miniMaxSum($arr);
+?>
