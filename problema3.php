6a7443dd2724e8886009058ac03f28122b5a3922
Jose Ruz <33108535+daraeowl@users.noreply.github.com>
2022-09-23 01:30:43 -0300

    Update problema3.php

--- problema3.php
+++ problema3.php
@@ -1,34 +1,26 @@
-<!--
-/*
-    Author: Jose Ruz
-    Email: daraeowl@outlook.com
+<?php
+//     Author: Jose Ruz
+//     Email: daraeowl@outlook.com
 
- Problema 3
+$arr = [5,5,5,10,15];
 
-Dado un array de enteros el desafío es encontrar un elemento del array tal que la suma de
-todos los elementos de la izquierda sea igual a la suma de todos los elementos de la
-derecha
-arr = [5, 6, 8, 11]
-Elemento target es 8 ya que la suma de los elementos de la izquierda es igual a la suma de
-todos los elementos de la derecha, es decir, 5 + 6 == 11
-arr = [2, 0, 0, 0]
-El elemento target es 2 ya que la suma de los elementos de la izquierda es cero y la suma
-de todos los elementos de la derecha es cero igualmente.
-arr = [1]
-El elemento target es 1.
+//target 15
+function balancedSums($arr) {
+    $sum = 0;
+    $right = 0;
+    $left = 0;
+    for ($i=0; $i < count($arr); $i++) { 
+        $sum += $arr[$i];
+    }
+    for ($i=0; $i < count($arr); $i++) { 
+        $right += $arr[$i];
+        if($right - $arr[$i] == $sum - $right){
+            return "YES";
+        }
+    }
+    return "NO";
+}
 
-Descripción de la función a desarrollar
-
-Debes completar la función balancedSums. Al final de la función debes retornar “YES” o
-“NO”
-“YES” = Es posible encontrar un elemento tal que la suma de todos los elementos de la
-izquierda sea igual a la de los elementos de la derecha.
-“NO” = No es posible encontrar un elemento tal que la suma de todos los elementos de la
-izquierda sea igual a la de los elementos de la derecha.
-Restricciones
-
-n = tamaño del array, 1 <= n <= 10^5
-arr[i] = valor de cada elemento, 1 <= arr[i] <= 2 * 10^4
-Formato de respuesta
-
-La función balancedSums debe retornar el string “YES” o “NO” en base a la descripción -->
+balancedSums($arr);
+print balancedSums($arr);
+?>
